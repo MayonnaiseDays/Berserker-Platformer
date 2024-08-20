21adfc588d8953b6618f5f5830244a6c030251d1
MayonnaiseDays <is.park2001@gmail.com>
2024-08-19 22:09:06 -0700

    script prep
    started work on enemy script
    ngl got tired so ill make more progress later

---
+++ Berserker Platformer/Assets/Scripts/Enemies/WalkingAimless.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkingAimless : MonoBehaviour
+{
+    float speed;
+    bool isFacingRight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 2f;
+        isFacingRight = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Move()
+    {
+
+    }
+
+    void cliffDetection()
+    {
+        
+    }
+
+    void hurtPlayer()
+    {
+
+    }
+}
